5dff06bf8001c5a239657f9e2208d101db099846
joshua-ashmore <114852555+joshua-ashmore@users.noreply.github.com>
2025-08-03 14:54:37 +0100

    fallback strategy

---
+++ components/strategies/fallback_strategy.py
@@ -0,0 +1,20 @@
+"""Fallback Strategy Job."""
+
+from datetime import date
+from typing import List, Literal
+from components.job.base_model import StrategyJob
+from components.strategies.base_model import Strategy
+from components.trades.intent_model import TradeIntent
+
+
+class FallbackStrategyJob(Strategy):
+    """Fallback Strategy Job."""
+
+    strategy_name: Literal["fallback"] = "fallback"
+    symbols: List[str] = []
+
+    def generate_signal_on_date(
+        self, job: StrategyJob, target_date: date, previous_date: date
+    ) -> List[TradeIntent]:
+        """Generate signals for all provided dates (or current_date if none given)."""
+        return []
