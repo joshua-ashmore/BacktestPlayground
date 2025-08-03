ccb1117ced609689249a8c7cb1e954ca43ebcc20
joshua-ashmore <114852555+joshua-ashmore@users.noreply.github.com>
2025-08-03 14:54:07 +0100

    include regime engine and per date runs to orchestrator

--- engine/orchestrator.py
+++ engine/orchestrator.py
@@ -1,15 +1,17 @@
 """Orchestrator."""
 
+from datetime import date
 from typing import Callable, List, Optional
 
 import pandas as pd
-from pydantic import BaseModel
+from pydantic import BaseModel, model_validator
 
 from backtester.market_data.market_data_feed import MarketFeed
 from backtester.market_data.sqlite_market_data_cache import SQLiteMetricsStore
 from components.backtester.all_backtesters import Backtesters
 from components.job.base_model import StrategyJob
 from components.metrics.base_model import MetricsEngine, PortfolioMetrics
+from components.regime.regime_engines import RegimeEngines
 from components.reporter.base_model import PDFReporter
 from components.strategies.all_strategies import Strategies
 
@@ -32,11 +34,33 @@ class OrchestratorConfig(BaseModel):
 
     job: StrategyJob
     market_feed: MarketFeed
-    strategy: Strategies
     backtester: Backtesters
     metrics_engine: MetricsEngine
+    strategy: Optional[Strategies] = None
+    regime_engine: Optional[RegimeEngines] = None
     reporter: Optional[PDFReporter] = None
+    all_tickers: List[str] = None
     hooks: List[Hook] = []
+
+    start_date: Optional[date] = None
+
+    @model_validator(mode="after")
+    def post_validate_model(self):
+        """Post validate model."""
+        if not self.strategy and not self.regime_engine:
+            raise ValueError("One of strategy or regime engine must be provided.")
+        if not self.all_tickers:
+            if self.strategy:
+                self.all_tickers = self.strategy.symbols
+            else:
+                self.all_tickers = [
+                    symbol
+                    for strategy in self.regime_engine.strategy_map.values()
+                    for symbol in strategy.symbols
+                ]
+        if not self.start_date:
+            self.start_date = min(self.market_feed.dates)
+        return self
 
 
 class Orchestrator(BaseModel):
@@ -45,11 +69,21 @@ class Orchestrator(BaseModel):
     config: OrchestratorConfig
 
     def _run_phase(
-        self, fn: Callable[[StrategyJob], None], stage: str, job: StrategyJob
+        self,
+        fn: Callable[[StrategyJob], None],
+        stage: str,
+        job: StrategyJob,
+        target_date: Optional[date] = None,
+        previous_date: Optional[date] = None,
     ):
         for hook in self.config.hooks:
             hook.before(stage=stage, job=job)
-        fn(job)
+        if target_date and previous_date:
+            fn(job=job, target_date=target_date, previous_date=previous_date)
+        elif target_date:
+            fn(job=job, target_date=target_date)
+        else:
+            fn(job=job)
         for hook in self.config.hooks:
             hook.after(stage=stage, job=job)
 
@@ -57,13 +91,42 @@ class Orchestrator(BaseModel):
         """Run strategy."""
         self._run_phase(fn=self._pre_save, stage="pre_save", job=self.config.job)
         self._run_phase(fn=self._load_data, stage="load_data", job=self.config.job)
-        self._run_phase(
-            fn=self._generate_signals, stage="generate_signals", job=self.config.job
-        )
-        self._run_phase(
-            fn=self._execute_trades, stage="execute_trades", job=self.config.job
-        )
-        self._run_phase(fn=self._run_backtest, stage="backtest", job=self.config.job)
+        dates = [
+            _date
+            for _date in self.config.market_feed.dates
+            if _date >= self.config.start_date
+        ]
+        for i, target_date in enumerate(dates):
+            if i == 0:
+                continue
+            previous_date = dates[i - 1]
+            self._run_phase(
+                fn=self._detect_regime,
+                stage="detect_regime",
+                job=self.config.job,
+                target_date=target_date,
+                previous_date=previous_date,
+            )
+            self._run_phase(
+                fn=self._generate_signals,
+                stage="generate_signals",
+                job=self.config.job,
+                target_date=target_date,
+                previous_date=previous_date,
+            )
+            self._run_phase(
+                fn=self._execute_trades,
+                stage="execute_trades",
+                job=self.config.job,
+                target_date=target_date,
+                previous_date=previous_date,
+            )
+            self._run_phase(
+                fn=self._run_backtest,
+                stage="backtest",
+                job=self.config.job,
+                target_date=target_date,
+            )
         self._run_phase(fn=self._compute_metrics, stage="metrics", job=self.config.job)
         self._run_phase(fn=self._save_results, stage="save", job=self.config.job)
         self._run_phase(fn=self._report, stage="report", job=self.config.job)
@@ -73,35 +136,54 @@ class Orchestrator(BaseModel):
         metrics_store = SQLiteMetricsStore()
         if self.config:
             metrics_store.save_strategy_config(
-                strategy_name=job.strategy_name, config=self.config
+                strategy_name=job.job_name, config=self.config
             )
 
     def _load_data(self, job: StrategyJob):
         """Load data."""
+        job.tickers = self.config.all_tickers
         self.config.market_feed.fetch_data(symbols=job.tickers)
+        job.market_snapshot = self.config.market_feed.market_snapshot
 
-    def _generate_signals(self, job: StrategyJob):
+    def _detect_regime(
+        self,
+        job: StrategyJob,
+        target_date: date,
+        previous_date: date,
+    ):
+        """Detect current market regime and assign strategy accordingly."""
+        if self.config.regime_engine:
+            self.config.strategy = self.config.regime_engine.detect_regime(
+                job=job,
+                target_date=target_date,
+                previous_date=previous_date,
+            )
+
+    def _generate_signals(
+        self, job: StrategyJob, target_date: date, previous_date: date
+    ):
         """Generate signals."""
-        job.signals = self.config.strategy.generate_signals(
-            job=job,
-            market_snapshot=self.config.market_feed.market_snapshot,
-            dates=self.config.market_feed.dates,
+        job.signals.extend(
+            self.config.strategy.generate_signal_on_date(
+                job=job, target_date=target_date, previous_date=previous_date
+            )
         )
 
-    def _execute_trades(self, job: StrategyJob):
+    def _execute_trades(self, job: StrategyJob, target_date: date, previous_date: date):
         """Run trade execution."""
-        job.equity_curve = self.config.backtester.simulate_trade_execution(
-            intents=job.signals,
-            market_snapshot=self.config.market_feed.market_snapshot,
-            dates=self.config.market_feed.dates,
+        job.equity_curve.extend(
+            self.config.backtester.simulate_trade_execution_on_date(
+                job=job,
+                target_date=target_date,
+                previous_date=previous_date,
+                strategy=self.config.strategy,
+            )
         )
 
-    def _run_backtest(self, job: StrategyJob):
+    def _run_backtest(self, job: StrategyJob, target_date: date):
         """Run backtest."""
-        job.portfolio_snapshots = self.config.backtester.run(
-            trades=job.equity_curve,
-            market_snapshot=self.config.market_feed.market_snapshot,
-            dates=self.config.market_feed.dates,
+        job.portfolio_snapshots.append(
+            self.config.backtester.run_on_date(job=job, target_date=target_date)
         )
 
     def _compute_metrics(self, job: StrategyJob):
@@ -111,6 +193,7 @@ class Orchestrator(BaseModel):
             market_snapshot=self.config.market_feed.market_snapshot,
             snapshots=job.portfolio_snapshots,
             trades=job.equity_curve,
+            regime_engine=self.config.regime_engine,
         )
 
     def _save_results(self, job: StrategyJob) -> PortfolioMetrics | None:
@@ -118,7 +201,7 @@ class Orchestrator(BaseModel):
         metrics_store = SQLiteMetricsStore()
         if job.metrics is not None and self.config.metrics_engine.save_metrics:
             metrics_store.save_portfolio_metrics(
-                metrics=job.metrics, strategy_name=job.strategy_name
+                metrics=job.metrics, strategy_name=job.job_name
             )
 
     def _report(self, job: StrategyJob):
@@ -128,4 +211,5 @@ class Orchestrator(BaseModel):
                 job=job,
                 market_snapshot=self.config.market_feed.market_snapshot,
                 metrics=job.metrics,
+                min_date=self.config.start_date,
             )
